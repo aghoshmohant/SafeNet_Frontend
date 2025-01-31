f55a49a090d76eefef42ac89e8be0e9956e073c6
Navas Moosa <kpnavasmoosa@gmail.com>
2025-01-31 10:50:39 +0530

    modified vehicle

--- config/axiosConfig.js
+++ config/axiosConfig.js
@@ -1,12 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-<<<<<<< HEAD
-  baseURL: 'http:// 192.168.20.:5000/api', // Adjust according to your server address
-=======
 
-  baseURL: 'http://192.168.169.34:5000/api', // Adjust according to your server address
->>>>>>> 6d09878541ca788fb43707e8d6c2780a03acf4f3
+  baseURL: 'http://192.168.20.23:5000/api', // Adjust according to your server address
   timeout: 10000,
 });
 
